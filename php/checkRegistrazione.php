c375203f6eee5e8de41aaaf543f845ea13836edc
salbeoo <82999092+salbeoo@users.noreply.github.com>
2022-03-30 12:48:44 +0200

    Update checkRegistrazione.php

--- php/checkRegistrazione.php
+++ php/checkRegistrazione.php
@@ -16,21 +16,14 @@ $password = md5($_POST["password"]);
 // echo $password;
 // $sql = "INSERT INTO utente (Username, Password, Nome,Cognome,DataDiNascita,Immagine) VALUES ('$username', '".md5($password)."', '$nome', '$cognome', '$datadinascita', '$target_file')";
 
-$sql = "SELECT  email FROM utente  where email = $email";
-$result = $conn->query($sql);
+$sql = $conn->prepare("INSERT INTO utente (nome, cognome, dataNascita,sesso,email,password) VALUES (?, ?, ?, ?, ?,?)");
+$sql->bind_param("ssssss", $nome, $cognome, $dataNascita, $genere, $email, $password);
 
-if ($result->num_rows) {
-  header("location:../html/registrazione.html?msg=ErroreRegistrazione");
+if ($sql->execute() === TRUE) {
+  header("location:../index.php");
 } else {
-  $sql = $conn->prepare("INSERT INTO utente (nome, cognome, dataNascita,sesso,email,password) VALUES (?, ?, ?, ?, ?,?)");
-  $sql->bind_param("ssssss", $nome, $cognome, $dataNascita, $genere, $email, $password);
-
-
-  if ($sql->execute() === TRUE) {
-    header("location:../index.php");
-  } else {
-    // header("location:registrazione.html?ErroreGenerico");
-  }
+  header("location:../html/register.html?ErroreGenerico");
 }
+
 $sql->close();
 $conn->close();
