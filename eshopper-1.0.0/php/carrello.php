e6cf345f56b69312b48e6d7784f91b3e14fd1e85
salbeoo <82999092+salbeoo@users.noreply.github.com>
2022-04-12 09:58:12 +0200

    SS

---
+++ eshopper-1.0.0/php/carrello.php
@@ -0,0 +1,57 @@
+<?php
+include("sessioni.php");
+include("connection.php");
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Amazon</title>
+
+    <link rel="stylesheet" type="text/css" href="html/css/util.css">
+    <link rel="stylesheet" type="text/css" href="html/css/main.css">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+</head>
+
+<body>
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="../index.php">Amazon</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+
+                <li class="nav-item active">
+                    <a class="nav-link" href="../html/login.html">Login</a>
+                </li>
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            </form>
+        </div>
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="carrello.php">Carrello</a>
+            </li>
+        </ul>
+    </nav>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+
+</html>
+
---
+++ eshopper-1.0.0/php/checkLogin.php
@@ -0,0 +1,31 @@
+<?php
+include("sessioni.php");
+include("connection.php");
+
+
+$email=$_POST["email"];
+$password=$_POST["password"];
+
+$sql = "SELECT  last(utente.id),email,carrello.id FROM utente join carrello on utente.id = carrello.idUtente where password = '".md5($password)."' and email = '$email' and pagato=null";
+$result = $conn->query($sql);
+
+if ($result->num_rows>0) {
+    // echo "New record created successfully";
+    $row = $result->fetch_assoc();
+    $_SESSION["idUtente"]=$row["utente.id"];
+    $_SESSION["email"]=$row["email"];
+    $_SESSION["idCarrello"]=$row["carrello.id"];
+    
+    setcookie("user_name", "guest", time() + (86400 * 30), "/"); // 86400 = 1 day
+    setcookie("utente_id", "-1", time() + (86400 * 30), "/"); // 86400 = 1 day
+    setcookie("utente_email", "prova@gmail.com", time() + (86400 * 30), "/"); // 86400 = 1 day
+    setcookie("carrello_id", "1000", time() + (86400 * 30), "/"); // 86400 = 1 day
+    
+    header("location:../index.php");
+  } else {
+    header("location:../html/login.html?ErroreLogin");
+  }
+  
+$conn->close();
+
+?>
